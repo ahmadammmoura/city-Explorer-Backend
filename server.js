be9bd85ea417c700f0d3a949fe16edee671027b8
ahmadammmoura <ammoura575@gmail.com>
2021-05-04 16:35:56 +0300

    finsh lab 7

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require('express')
 var cors = require('cors')
 const app = express()
-const data = require('./assets/data.json')
+const wather = require('./assets/data.json')
 require('dotenv').config()
 const port = process.env.PORT
 app.use(cors())
 
 app.get('/weather', function (req, res) {
-  res.send(data);
+  const arrayOfData = wather.data.map(data => new Weather(data));
+  console.log(arrayOfData)
+  res.send(arrayOfData);
 });
+
+class Weather {
+  constructor(data){
+    this.description = data.weather.description,
+    this.date = data.valid_date
+  }
+}
 
  
 app.listen(port)
