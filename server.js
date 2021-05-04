b96f153fea5d25940c8e573ad53d2b8ee1dd3eaf
ahmadammmoura <ammoura575@gmail.com>
2021-05-04 21:45:03 +0300

    done 08% lab 08

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const express = require('express')
 var cors = require('cors')
+const superagent = require('superagent');
 const app = express()
 const wather = require('./assets/data.json')
 require('dotenv').config()
 const port = process.env.PORT
+const key = process.env.WEATHER_BIT_KEY
 app.use(cors())
 
 app.get('/weather', function (req, res) {
-  const arrayOfData = wather.data.map(data => new Weather(data));
-  console.log(arrayOfData)
-  res.send(arrayOfData);
+  try{
+  const weatherBitUrl = `https://api.weatherbit.io/v2.0/forecast/daily?key=${key}&lat=${req.query.lat}&lon=${req.query.lon}`
+  const weatherData = superagent.get(weatherBitUrl)
+  .then(city => city.body.data )
+  .then(data => {
+    const arrayOfData = data.map(city => new Weather(city));
+    res.send(arrayOfData);
+  })
+}catch (error) {
+  res.send(error)
+}
 });
 
 class Weather {
